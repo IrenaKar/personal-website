3a5670f8a9ecf4dae44d764e68af40bf52d53362
Irena Karamachoska <irena.karamacoska@yahoo.com>
2023-06-15 23:09:22 +0200

    fix: fixed scrolling offset issues

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -2,18 +2,21 @@ import { FormEvent, MutableRefObject } from "react";
 import emailjs from "@emailjs/browser";
 
 export const onScrollSection = (
-  e: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+  e: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  headerElement: HTMLElement | null
 ): void => {
   e.preventDefault();
   const target = window.document.getElementById(
     e.currentTarget.href.split("#")[1]
   );
+  const headerHeight = headerElement?.offsetHeight;
   if (target) {
-    const targetPosition = target.getBoundingClientRect().top + 5;
+    const targetPosition =
+      target.getBoundingClientRect().top - (headerHeight ?? 0);
     const offsetPosition = targetPosition;
     window.scrollTo({
-      top: window.pageYOffset + offsetPosition,
-      behavior: "smooth"
+      top: window.window.scrollY + offsetPosition,
+      behavior: "smooth",
     });
   }
 };
@@ -39,7 +42,6 @@ export const sendEmail = (
         console.log(error.text);
       }
     );
-
   if (e.target instanceof HTMLFormElement) {
     e.target.reset();
   }
